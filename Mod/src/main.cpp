ad34205aa075c64155f21e240758e5018fff0073
oki3ed <kokosik3215@gmail.com>
2025-07-28 20:14:44 +0300

    Implementation of new settings

--- Mod/src/main.cpp
+++ Mod/src/main.cpp
@@ -7,12 +7,41 @@ class $modify(MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) return false;
 
-        if (auto menu = this->getChildByID("social-media-menu")) {
-            if (auto twitchBtn = menu->getChildByID("twitch-button"))
-                twitchBtn->setVisible(false);
+        auto version = Mod::get()->getSettingValue<std::string>("version");
+        auto robtopLogo = Mod::get()->getSettingValue<bool>("robtop-logo");
 
-            if (auto discordBtn = menu->getChildByID("discord-button"))
-                discordBtn->setVisible(false);
+        if (auto menu = this->getChildByID("social-media-menu")) {
+            if (version == "1.9 - 2.1") {
+                if (auto twitchBtn = menu->getChildByID("twitch-button"))
+                    twitchBtn->setVisible(false);
+                if (auto discordBtn = menu->getChildByID("discord-button"))
+                    discordBtn->setVisible(false);
+            }
+            else if (version == "1.3 - 1.8") {
+                if (auto youtubeBtn = menu->getChildByID("youtube-button"))
+                    youtubeBtn->setVisible(false);
+                if (auto twitchBtn = menu->getChildByID("twitch-button"))
+                    twitchBtn->setVisible(false);
+                if (auto discordBtn = menu->getChildByID("discord-button"))
+                    discordBtn->setVisible(false);
+            }
+            else if (version == "1.0 - 1.2") {
+                if (auto facebookBtn = menu->getChildByID("facebook-button"))
+                    facebookBtn->setVisible(false);
+                if (auto twitterBtn = menu->getChildByID("twitter-button"))
+                    twitterBtn->setVisible(false);
+                if (auto youtubeBtn = menu->getChildByID("youtube-button"))
+                    youtubeBtn->setVisible(false);
+                if (auto twitchBtn = menu->getChildByID("twitch-button"))
+                    twitchBtn->setVisible(false);
+                if (auto discordBtn = menu->getChildByID("discord-button"))
+                    discordBtn->setVisible(false);
+            }
+
+            if (!robtopLogo) {
+                if (auto robTopLogoBtn = menu->getChildByID("robtop-logo-button"))
+                    robTopLogoBtn->setVisible(false);
+            }
         }
 
         return true;
